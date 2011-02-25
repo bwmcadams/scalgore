83218fc7261caff1f578c7474a01376fd4f461a4
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-02-26 00:30:15 +0100

    update all dependencies to the latest version

--- project/build/ScalGore.scala
+++ project/build/ScalGore.scala
@@ -2,20 +2,18 @@ import sbt._
 
 class ScalGoreProject(info: ProjectInfo) extends DefaultProject(info) {
   override val mainClass = Some("net.evilmonkeylabs.scalgore.ScalGore")
-  val akka = "se.scalablesolutions.akka" % "akka-core_2.8.0.RC3" % "0.9"
-  val elasticSearch = "org.elasticsearch" % "elasticsearch" % "0.8.0"
+
+  val elasticSearch = "org.elasticsearch" % "elasticsearch" % "0.15.0"
   val commonsHTTP = "commons-httpclient" % "commons-httpclient" % "3.1"
-  val akkaPersistence  = "se.scalablesolutions.akka" % "akka-persistence_2.8.0.RC3" % "0.9"
-  val scalaToolsRepo = ScalaToolsReleases
-  val scalaToolsSnaps = ScalaToolsSnapshots
+  
+  val casbah = "com.mongodb.casbah" %% "casbah" % "2.0.3"
+  val akka_actor = "se.scalablesolutions.akka" % "akka-actor" % "1.0"
+  val configgy = "net.lag" % "configgy" % "2.0.2"
+  
+  val twitterRep = "Twitter" at "http://maven.twttr.com"
   val sonaRepo = "Sona Repo" at "http://oss.sonatype.org/content/repositories/releases/"
-  val sunRepo = "Sun Repository" at "http://download.java.net/maven/2/"
-  val akkaRepo = "Akka Repository" at "http://www.scalablesolutions.se/akka/repository"
-  val guiceRepo = "Guice Repo" at "http://guiceyfruit.googlecode.com/svn/repo/releases"
-  val ibiblio = "Ibiblio Maven" at "http://mirrors.ibiblio.org/pub/mirrors/maven2"
+  val akkaRepo = "Akka Repository" at "http://www.akka.io/repository"
   val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
-  //val thirdPartyMaven = "Local Third Party Maven Repository (Per Project)" at "file://   " + path("third-party.mvn").absString 
-  //val jbossRepo = "JBoss Repo" at "http://repository.jboss.org/nexus/content/groups/public"
 }
 
 // vim: set ts=2 sw=2 sts=2 et:
