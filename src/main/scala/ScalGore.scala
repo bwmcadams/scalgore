e3b5345534fd63a98381b95e23dca2b9dbf187ba
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-02-26 16:06:01 +0100

    better naming of config properties

--- src/main/scala/ScalGore.scala
+++ src/main/scala/ScalGore.scala
@@ -6,24 +6,25 @@
 package net.evilmonkeylabs.scalgore
 
 import org.jibble.pircbot._
-import net.lag.configgy.Configgy
-import akka.actor._
+import net.lag.configgy.{Config=>CConfig}
+import akka.actor.Actor.actorOf
 import akka.util._
+import akka.camel._
 
 object Config {
-	val servers = "scalgore.servers"
+	val networks = "scalgore.networks"
 
-	val uri: (String)=>String = "scalgore.%s.uri" format _
+	val host: (String)=>String = "scalgore.%s.host" format _
 	val channels: (String)=>String = "scalgore.%s.channels" format _
+	val nick: (String)=>String = "scalgore.%s.nick" format _
 }
 
-case class Connection(name: String, uri: String, channels: Seq[String])
+case class Network(name: String, host: String, nick: String, channels: Seq[String])
 
 object ScalGore extends Logging {
 
   def main(args: Array[String]) {
-  	Configgy.configureFromResource("scalgore.conf", getClass.getClassLoader)
-    val bot = new IrcBot("ScalGore", true)
+    val cfg = CConfig.fromResource("scalgore.conf", getClass.getClassLoader)
 
     val cfg = Configgy.config
 
