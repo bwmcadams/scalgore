692581d0e0672cf783871418afb13ff5c1abbd30
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-02-26 10:09:09 +0100

    use akka-sbt-plugin and add akka-camel dep

--- project/build/ScalGore.scala
+++ project/build/ScalGore.scala
@@ -1,19 +1,26 @@
 import sbt._
 
-class ScalGoreProject(info: ProjectInfo) extends DefaultProject(info) {
+class ScalGoreProject(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
   override val mainClass = Some("net.evilmonkeylabs.scalgore.ScalGore")
+
+  val TwitterRepo = MavenRepository("Twitter Repository", "http://maven.twttr.com")
+  val MavenLocal = MavenRepository("Local Maven Repository", "file://" + Path.userHome + "/.m2/repository")
+  val SonatypeReleasesRepo = MavenRepository("Sonatype OSS Repo", "http://oss.sonatype.org/content/repositories/releases")
+
+
+  val configgyConfig = ModuleConfiguration("net.lag.configgy", TwitterRepo)
+  val elasticConfig = ModuleConfiguration("org.elasticsearch", SonatypeReleasesRepo)
+
 
   val elasticSearch = "org.elasticsearch" % "elasticsearch" % "0.15.0"
   val commonsHTTP = "commons-httpclient" % "commons-httpclient" % "3.1"
-  
-  val casbah = "com.mongodb.casbah" %% "casbah" % "2.0.3"
-  val akka_actor = "se.scalablesolutions.akka" % "akka-actor" % "1.0"
+
+  // we already have akka-actor from AkkaProject
+  val akka_camel = akkaModule("camel")
+
   val configgy = "net.lag" % "configgy" % "2.0.2"
-  
-  val twitterRep = "Twitter" at "http://maven.twttr.com"
-  val sonaRepo = "Sona Repo" at "http://oss.sonatype.org/content/repositories/releases/"
-  val akkaRepo = "Akka Repository" at "http://www.akka.io/repository"
-  val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
+  val casbah = "com.mongodb.casbah" %% "casbah" % "2.0.3"
+
 }
 
 // vim: set ts=2 sw=2 sts=2 et:
